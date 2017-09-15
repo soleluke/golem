e448c5f482e1f0fb4fcd10a7e6352488687a42ec
Unknown <ebersoleluke@gmail.com>
2017-09-14 23:01:49 -0500

    bugfix for Golem responding to itself

--- source/main.py
+++ source/main.py
@@ -23,17 +23,7 @@ async def on_ready():
     print('-------')
 @client.event
 async def on_message(message):
-    sender ="<@"+ message.author.id+">"
-    tells = commands.get_tells(sender)
-    for tell in tells:
-        tmp = await client.send_message(message.channel,tell)
-    if message.content.startswith(config.get_prefix()):
-        command = get_command(message.content)
-        args = message.content.split(' ',1)[1]
-        if config.check_command(command):
-            tmp = await client.send_message(message.channel,commands.get_response(sender,command,args))
-        else:
-            tmp = await client.send_message(message.channel,commands.get_unsupported_msg(command))
+    if message.author != client.user:
         tmp = await check_triggers(message)
 
 async def check_triggers(message):
